4e7d90ebe383dbdfe9e92469f393f594b6fb22b3
Maiio268 <mmonber326@g.educaand.es>
2025-04-08 22:17:41 +0200

    commit

---
+++ js/Fase_2/formulario_validacion.js
@@ -0,0 +1,8 @@
+/* Para validar el formulario, primero vamos a seleccionar todos los elementos del formulario y vamos a asignarlos a variables, después crearé las funciones para validar cada cosa */
+const formulario = document.querySelector(".formulario");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const telefono = document.getElementById("telefono");
+const direccion = document.getElementById("direccion");
+const codigoPostal = document.getElementById("codigo_postal");
+const genero = document.getElementById("genero");
